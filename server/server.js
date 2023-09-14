b8615044f4f33d17cfe6c854f6b8f8f24342420a
hursittarcan <hursittarcan@gmail.com>
2023-09-14 15:22:04 +0300

    setup stripe in server

--- server/server.js
+++ server/server.js
@@ -12,8 +12,18 @@ const storeItems = new Map([
 app.use(express.json());
 app.use(express.static("public"));
 
-app.post('/create-checkout-session', (req, res) => {
-    res.json({ url: 'post-works'});
+app.post('/create-checkout-session', async (req, res) => {
+    try {
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ["card"],
+            mode: 'payment',
+            success_url: `${process.env.SERVER_URL}/success.html`,
+            cancel_url:`${process.env.SERVER_URL}/cancel.html`
+        })
+        res.json({ url: session.url });
+    } catch(e) {
+        res.status(500).json({ error: e.message });
+    }
 });
 
 app.listen(3000);
