4ea05ab2f184272d13bdd0166d3767e1cc5862e6
hursittarcan <hursittarcan@gmail.com>
2023-09-14 15:31:42 +0300

    stripe product setup done

--- server/server.js
+++ server/server.js
@@ -17,6 +17,19 @@ app.post('/create-checkout-session', async (req, res) => {
         const session = await stripe.checkout.sessions.create({
             payment_method_types: ["card"],
             mode: 'payment',
+            line_items: req.body.items.map(item => {
+                const storeItem = storeItems.get(item.id);
+                return {
+                    price_data: {
+                        currency: 'eur',
+                        product_data: {
+                            name: storeItem.name
+                        },
+                        unit_amount: storeItem.priceInCents
+                    },
+                    quantity: item.quantity
+                }
+            }),
             success_url: `${process.env.SERVER_URL}/success.html`,
             cancel_url:`${process.env.SERVER_URL}/cancel.html`
         })
