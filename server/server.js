0e7fd408a1a0364050c6cf607a4f52d607c998ac
hursittarcan <hursittarcan@gmail.com>
2023-09-14 14:25:09 +0300

    nodejs express server runs!

---
+++ server/server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const express = require('express');
+const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
+const app = express();
+
+const storeItems = new Map([
+    [1, { priceInCents: 10000, name: "Learn React Today" }],
+    [2, { priceInCents: 20000, name: "Learn CSS Today" }],
+]);
+
+app.use(express.json());
+app.listen(3000);
